e3324d41e4d824b69430d3ddaf1fab64d8d73b45
Bruce Stockwell <bruce.stockwell@gmail.com>
2020-08-09 19:12:57 -0400

    installed vdom and did 'hello world'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./index.css";
+import { init } from "snabbdom/build/package/init";
+import { classModule } from "snabbdom/build/package/modules/class";
+import { propsModule } from "snabbdom/build/package/modules/props";
+import { styleModule } from "snabbdom/build/package/modules/style";
+import { eventListenersModule } from "snabbdom/build/package/modules/eventlisteners";
+import { h } from "snabbdom/build/package/h"; // helper function for creating vnodes
 
-alert("connected");
+const patch = init([
+  classModule,
+  propsModule,
+  styleModule,
+  eventListenersModule,
+]);
+const container = document.getElementById("root");
+console.log("container", container);
+const vnode = h("div", { style: { fontWeight: "bold" } }, "Hello World");
+patch(container, vnode);

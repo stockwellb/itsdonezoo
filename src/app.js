c45d4c804a652fa40a0269950d48e3a7925634ed
Bruce Stockwell <bruce.stockwell@gmail.com>
2020-08-10 00:00:57 -0400

    set up JSX correctly ( I hope )

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
-/**  @jsx h */
-import { h } from "snabbdom/build/package/h";
+import Snabbdom from "snabbdom-pragma";
+const handleClick = (e) => alert(process.env.GREETING);
+const app = (
+  <div>
+    <h1 style={{ fontWeight: "bold" }}>{process.env.GREETING}</h1>
+    <button type="button" on-click={handleClick}>
+      Hey
+    </button>
+  </div>
+);
 
-const app = <h1 style={{ fontWeight: "bold" }}>{process.env.GREETING}</h1>;
+console.log("app", app);
 
 export default app;

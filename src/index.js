f10a2b90cbb3d1c01357e9db2c4240410b0eb26e
Bruce Stockwell <bruce.stockwell@gmail.com>
2020-08-09 23:10:40 -0400

    added JSX support ( I think )

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+/**  @jsx h */
+import { h } from "snabbdom/build/package/h";
 import "./index.css";
 import { init } from "snabbdom/build/package/init";
 import { classModule } from "snabbdom/build/package/modules/class";
 import { propsModule } from "snabbdom/build/package/modules/props";
 import { styleModule } from "snabbdom/build/package/modules/style";
 import { eventListenersModule } from "snabbdom/build/package/modules/eventlisteners";
-import { h } from "snabbdom/build/package/h"; // helper function for creating vnodes
 
 const patch = init([
   classModule,
@@ -14,5 +15,5 @@ const patch = init([
 ]);
 
 const container = document.getElementById("root");
-const vnode = h("div", { style: { fontWeight: "bold" } }, process.env.GREETING);
-patch(container, vnode);
+const app = <h1 style={{ fontWeight: "bold" }}>{process.env.GREETING}</h1>;
+patch(container, app);

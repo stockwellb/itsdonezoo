25f279d94624749d0b664ae9008abac75379db56
Bruce Stockwell <bruce.stockwell@gmail.com>
2020-08-11 20:12:47 -0400

    moving stuff around

--- src/app.js
+++ src/app.js
@@ -4,11 +4,6 @@ export default ({}, children) => (
   <section>
     <div>{children}</div>
     <div style={{ margin: "16px" }}>
-      <h1>{process.env.APP_TITLE}</h1>
-      <p>{process.env.APP_CAPTION}</p>
-      {process.env.APP_ENVIRONMENT && (
-        <span>({process.env.APP_ENVIRONMENT})</span>
-      )}
       <div id="content"></div>
     </div>
   </section>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,6 +3,11 @@ import Snabbdom from "snabbdom-pragma";
 const handleClick = (e) => alert("Thank you I needed that.");
 export default () => (
   <section id="content">
+    <h1>{process.env.APP_TITLE}</h1>
+    <p>{process.env.APP_CAPTION}</p>
+    {process.env.APP_ENVIRONMENT && (
+      <span>({process.env.APP_ENVIRONMENT})</span>
+    )}
     <h2>Home</h2>
     <p>
       Maybe some dashboard stuff could go here. Total number of lists, total

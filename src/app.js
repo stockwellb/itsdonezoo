f13666b5d836d89ce33086a9ec365729709d5b5f
Bruce Stockwell <bruce.stockwell@gmail.com>
2020-08-11 20:10:48 -0400

    just some fun style stuff

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import Snabbdom from "snabbdom-pragma";
 export default ({}, children) => (
   <section>
     <div>{children}</div>
-    <h1>{process.env.APP_TITLE}</h1>
-    <p>{process.env.APP_CAPTION}</p>
-    {process.env.APP_ENVIRONMENT && (
-      <span>({process.env.APP_ENVIRONMENT})</span>
-    )}
-    <div id="content"></div>
+    <div style={{ margin: "16px" }}>
+      <h1>{process.env.APP_TITLE}</h1>
+      <p>{process.env.APP_CAPTION}</p>
+      {process.env.APP_ENVIRONMENT && (
+        <span>({process.env.APP_ENVIRONMENT})</span>
+      )}
+      <div id="content"></div>
+    </div>
   </section>
 );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,30 @@
 import Snabbdom from "snabbdom-pragma";
 
+const style = {
+  margin: "16px",
+  display: "block",
+  flex: "0 1 auto",
+  listStyleType: "none",
+};
 export default ({}, children) => (
-  <nav>
-    <ul>
-      <li>
+  <nav style={{ backgroundColor: "lightgrey" }}>
+    <ul
+      style={{
+        display: "flex",
+        justifyContent: "flex-start",
+        width: "100%",
+      }}
+    >
+      <li style={style}>
         <a href="#">Home</a>
       </li>
-      <li>
+      <li style={style}>
         <a href="/#/about">About</a>
       </li>
-      <li>
+      <li style={style}>
         <a href="/#/profile">Profile</a>
       </li>
-      <li>
+      <li style={style}>
         <a href="/#/lists">Lists</a>
       </li>
     </ul>
